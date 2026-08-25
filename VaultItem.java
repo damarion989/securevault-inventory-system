e1a6e4f85dadc8958f2d2f393085c3ec303d28ec
damarion989 <d.butlers282@gmail.com>
2026-08-24 21:04:07 -0500

    Update VaultItem.java

--- VaultItem.java
+++ VaultItem.java
@@ -1,1 +1,23 @@
+package com.securevault.core;
 
+public class VaultItem {
+  private String itemName;
+  private double itemValue;
+
+  public VaultItem(String itemName, double itemValue) {
+    this.ItemName = ItemName;
+    this.ItemValue = ItemValue;
+  }
+
+public String getItemName() {
+  return itemName;
+}
+
+public double getItemValue() {
+  return itemValue;
+}
+
+@Override
+public String toString() {
+  return "Item: " + itemName + ", Value: $" + itemValue;
+}
