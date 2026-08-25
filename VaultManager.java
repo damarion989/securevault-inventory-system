db22606f065e1afc4441e898415daa64c4a45438
damarion989 <d.butlers282@gmail.com>
2026-08-24 21:44:25 -0500

    Update VaultManager.java

--- VaultManager.java
+++ VaultManager.java
@@ -1,1 +1,22 @@
+package com.securevault.core;
 
+public class VaultManager {
+
+    public void selectionSort(VaultItem[] items) {
+        int n = items.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (items[j].getItemValue() < items[minIndex].getItemValue()) {
+                    minIndex = j;
+                }
+            }
+
+            VaultItem temp = items[minIndex];
+            items[minIndex] = items[i];
+            items[i] = temp;
+        }
+    }
+}
