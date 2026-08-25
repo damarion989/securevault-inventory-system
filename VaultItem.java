fe804f7fed2ff6469768c8e0d74ac768fe7c281a
damarion989 <d.butlers282@gmail.com>
2026-08-24 21:35:32 -0500

    Enhance VaultItem with value validation
    Updated VaultItem constructor to include validation for item value. Added exception handling for invalid item values.

--- VaultItem.java
+++ VaultItem.java
@@ -1,23 +1,27 @@
 package com.securevault.core;
 
 public class VaultItem {
-  private String itemName;
-  private double itemValue;
+    private String itemName;
+    private double itemValue;
 
-  public VaultItem(String itemName, double itemValue) {
-    this.ItemName = ItemName;
-    this.ItemValue = ItemValue;
-  }
+    public VaultItem(String itemName, double itemValue) throws InvalidItemValueException {
+        if (itemValue <= 0) {
+            throw new InvalidItemValueException("Item value must be greater than $0. Received: $" + itemValue);
+        }
+        this.itemName = itemName;
+        this.itemValue = itemValue;
+    }
 
-public String getItemName() {
-  return itemName;
-}
+    public String getItemName() {
+        return itemName;
+    }
 
-public double getItemValue() {
-  return itemValue;
-}
+    public double getItemValue() {
+        return itemValue;
+    }
 
-@Override
-public String toString() {
-  return "Item: " + itemName + ", Value: $" + itemValue;
+    @Override
+    public String toString() {
+        return "Item: " + itemName + ", Value: $" + itemValue;
+    }
 }
