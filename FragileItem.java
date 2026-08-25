d686a8cf33c62538aabd391441ce362f1d53355d
damarion989 <d.butlers282@gmail.com>
2026-08-24 21:37:19 -0500

    Enhance FragileItem with error handling and fixes
    Refactor FragileItem class to include exception handling for item value and fix return statement.

--- FragileItem.java
+++ FragileItem.java
@@ -1,22 +1,22 @@
 package com.securevault.specialty;
 
 import com.securevault.core.VaultItem;
+import com.securevault.core.InvalidItemValueException;
 
 public class FragileItem extends VaultItem {
-  private boolean isWrapped;
+    private boolean isWrapped;
 
-  public FragileItem(String itemName, double itemValue, boolean isWrapped) {
-    super(itemName, itemValue);
-    this.isWrapped = isWrapped;
-  }
+    public FragileItem(String itemName, double itemValue, boolean isWrapped) throws InvalidItemValueException {
+        super(itemName, itemValue);
+        this.isWrapped = isWrapped;
+    }
 
-public boolean isWrapped() {
-  returen isWrapped;
-}
+    public boolean isWrapped() {
+        return isWrapped;
+    }
 
-
-@Override
-public String toString() {
-  return "Item: " + getItemName() + ", Value: $" + getItemValue() + ", Wrapped: " + isWrapped;
+    @Override
+    public String toString() {
+        return "Item: " + getItemName() + ", Value: $" + getItemValue() + ", Wrapped: " + isWrapped;
     }
 }
